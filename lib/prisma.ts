abfd1015347558d0dbc78c5981aa6e5523ba7269
Taras Ishchuk <ishchuktaras@gmail.com>
2025-06-22 11:37:11 +0200

    prisma logs add

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,15 +5,29 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-let prisma: PrismaClient | undefined
+let prisma: PrismaClient
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
+  console.log("Initializing PrismaClient in production...") 
+  try {
+    prisma = new PrismaClient()
+    console.log("PrismaClient initialized successfully in production.") 
+  } catch (e) {
+    console.error("Error initializing PrismaClient in production:", e) 
+    throw e // Znovu vyhodit chybu, aby se projevila
+  }
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    console.log("Initializing PrismaClient in development (global instance)...") 
+    try {
+      global.prisma = new PrismaClient()
+      console.log("PrismaClient initialized successfully in development.") 
+    } catch (e) {
+      console.error("Error initializing PrismaClient in development:", e) 
+      throw e
+    }
   }
-  prisma = global.prisma
+  prisma = global.prisma as PrismaClient
 }
 
 export default prisma
